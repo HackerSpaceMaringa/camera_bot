4367141bd5c4b729d26e773ba09ca2f27ecc081f
Chicharme Guilhernoski <guilherme.chichanoski@gmail.com>
2020-01-14 23:46:37 -0300

    add webserver to listen to webhooks

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
+#[macro_use]
+extern crate lazy_static;
+
+use actix_web::{middleware, web, App, HttpResponse, HttpServer};
 use futures::prelude::*;
 use serde::Deserialize;
 use std::env;
 use telegram_bot::prelude::*;
 use telegram_bot::{Api, UpdateKind};
+
+static mut HS_OPEN: bool = false;
+
+lazy_static! {
+    static ref BASE_URL: String = format!(
+        "http://{host}:{port}/{token}",
+        host = env::var("SHINOBI_HOST").expect("SHINOBI_HOST is required"),
+        port = env::var("SHINOBI_PORT").unwrap_or_else(|_| "8080".to_string()),
+        token = env::var("SHINOBI_TOKEN").expect("SHINOBI_TOKEN is required")
+    );
+    static ref TOKEN: String = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+    static ref API: telegram_bot::Api = Api::new(TOKEN.clone());
+    static ref GROUP_KEY: String =
+        env::var("SHINOBI_GROUP_KEY").expect("SHINOBI_GROUP_KEY is required");
+    static ref ID_GROUP: telegram_bot::types::refs::GroupId =
+        telegram_bot::types::refs::GroupId::new(
+            env::var("ID_GROUP")
+                .expect("ID_GROUP is required")
+                .parse::<telegram_bot::types::primitive::Integer>()
+                .expect("ID_GROUP not an integer")
+        );
+}
 
 #[derive(Debug)]
 struct Error {
@@ -12,6 +38,14 @@ struct Error {
 #[derive(Deserialize, Debug)]
 struct Monitor {
     mid: String,
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_: std::io::Error) -> Self {
+        Error {
+            message: "IO deu ruim".to_string(),
+        }
+    }
 }
 
 impl From<telegram_bot::Error> for Error {
@@ -30,20 +64,56 @@ impl From<reqwest::Error> for Error {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+async fn index() -> HttpResponse {
+    let chat = telegram_bot::types::chat::MessageChat::Group(telegram_bot::types::chat::Group {
+        id: *ID_GROUP,
+        title: "Group".to_string(),
+        all_members_are_administrators: false,
+        invite_link: None,
+    });
+    if unsafe { !HS_OPEN } {
+        if send_photos_to_chat(&chat).await.is_ok() {
+            HttpResponse::Ok().finish()
+        } else {
+            HttpResponse::InternalServerError().finish()
+        }
+    } else {
+        HttpResponse::Ok().finish()
+    }
+}
 
-    let base_url = format!(
-        "http://{host}:{port}/{token}",
-        host = env::var("SHINOBI_HOST").expect("SHINOBI_HOST is required"),
-        port = env::var("SHINOBI_PORT").unwrap_or_else(|_| "8080".to_string()),
-        token = env::var("SHINOBI_TOKEN").expect("SHINOBI_TOKEN is required")
-    );
-    let group_key = env::var("SHINOBI_GROUP_KEY").expect("SHINOBI_GROUP_KEY is required");
+async fn send_photos_to_chat(chat: &telegram_bot::types::MessageChat) -> Result<(), Error> {
+    let monitors: Vec<Monitor> = reqwest::get(&format!(
+        "{base_url}/smonitor/{group_key}",
+        base_url = *BASE_URL,
+        group_key = *GROUP_KEY
+    ))
+    .await?
+    .json()
+    .await?;
 
-    let api = Api::new(token);
-    let mut stream = api.stream();
+    for monitor in &monitors {
+        let bytes = reqwest::get(&format!(
+            "{base_url}/jpeg/{group_key}/{monitor_id}/s.jpg",
+            base_url = *BASE_URL,
+            group_key = *GROUP_KEY,
+            monitor_id = monitor.mid
+        ))
+        .await?
+        .bytes()
+        .await?;
+
+        API.send(chat.photo(telegram_bot::types::InputFileUpload::with_data(
+            bytes, "teste",
+        )))
+        .await?;
+    }
+
+    Ok(())
+}
+
+async fn bot() -> Result<(), Error> {
+    let mut stream = API.stream();
 
     while let Some(update) = stream.next().await {
         if let UpdateKind::Message(telegram_bot::types::message::Message {
@@ -57,37 +127,33 @@ async fn main() -> Result<(), Error> {
                 if entity.kind == telegram_bot::types::message::MessageEntityKind::BotCommand {
                     let command = &data.as_str()
                         [entity.offset as usize..entity.offset as usize + entity.length as usize];
-                    if command == "/photo" {
-                        let monitors: Vec<Monitor> = reqwest::get(&format!(
-                            "{base_url}/smonitor/{group_key}",
-                            base_url = base_url,
-                            group_key = group_key
-                        ))
-                        .await?
-                        .json()
-                        .await?;
-
-                        for monitor in &monitors {
-                            let bytes = reqwest::get(&format!(
-                                "{base_url}/jpeg/{group_key}/{monitor_id}/s.jpg",
-                                base_url = base_url,
-                                group_key = group_key,
-                                monitor_id = monitor.mid
-                            ))
-                            .await?
-                            .bytes()
-                            .await?;
-
-                            api.send(chat.photo(telegram_bot::types::InputFileUpload::with_data(
-                                bytes, "teste",
-                            )))
-                            .await?;
-                        }
+                    match command {
+                        "/photo" => send_photos_to_chat(&chat).await?,
+                        "/open" => unsafe { HS_OPEN = true },
+                        "/close" => unsafe { HS_OPEN = false },
+                        _ => {}
                     }
                 }
             }
         }
     }
+
+    Ok(())
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), Error> {
+    futures::try_join!(
+        HttpServer::new(|| {
+            App::new()
+                .wrap(middleware::Logger::default())
+                .service(web::resource("/").to(index))
+        })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .err_into(),
+        bot()
+    )?;
 
     Ok(())
 }
